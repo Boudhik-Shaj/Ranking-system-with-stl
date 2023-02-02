e1fd90a80c28cf98cb253db8a1ddb66220170906
Boudhik Shaj <82608513+Boudhik-Shaj@users.noreply.github.com>
2023-02-02 13:48:53 +0530

    qwertyun nhytr

--- rsws.cpp
+++ rsws.cpp
@@ -3,36 +3,40 @@
 #include <algorithm>
 using namespace std;
 
-struct marks {
-    int id, mark;
+// struct marks {
+//     int id, mark;
  
-    // Constructor
-    marks(int id, int mark)
-    {
-        this->id = id;
-        this->mark = mark;
-    }
-};
+//     // Constructor
+//     marks(int id, int mark)
+//     {
+//         this->id = id;
+//         this->mark = mark;
+//     }
+// };
 
-bool cmp(struct marks a, struct marks b)
-{
-    int r1=a.mark;
-    int r2=b.mark;
-    return r1 > r2;
+// bool cmp(struct marks a, struct marks b)
+// {
+//     int r1=a.mark;
+//     int r2=b.mark;
+//     return r1 > r2;
+// }
+bool cmp(const pair<int,int> &a,const pair<int,int> &b){
+    return (a.second < b.second);
 }
 
-int ranking(struct vector<int>& marks,int n){
-  sort(marks.begin(), marks.end(), cmp);
+// int ranking(struct vector<int>& marks,int n){
   
-  return 0;
-}
+  
+//   return 0;
+// }
 
 
 int main(){
   vector<vector<int>> marks{ {20301, 72},{20302, 56 },{20303, 94 } };// 2d-vector created 
   int n=marks.size();
-  ranking(marks,n); 
-  //error  reference of type "std::vector<int, std::allocator<int>> &" (not const-qualified) cannot be initialized with a value of type "std::vector<std::vector<int, std::allocator<int>>, std::allocator<std::vector<int, std::allocator<int>>>>"C/C++(434)
+  // ranking(marks,n); 
+  sort(marks.begin(), marks.end(), cmp);
+
   for (int i = 0; i < marks.size(); i++) {
         for (int j = 0; j < marks[i].size(); j++)
             cout << marks[i][j] << " ";
