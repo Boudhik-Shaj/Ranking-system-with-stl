9d457cd9192a4400a786591869c93d14bb2b3551
Boudhik Shaj <82608513+Boudhik-Shaj@users.noreply.github.com>
2023-02-03 11:51:41 +0530

    menu format added

--- new_code.cpp
+++ new_code.cpp
@@ -42,35 +42,48 @@ int main() {
         {20307,9,8,7},
         {20308,6,5,8},
         {20309,10,9,9},
-        {20310,8,7,9}   
+        {20310,8,7,9} 
+      
  };
 
   sort(students.begin(), students.end(), compareStudents);
-  cout<<"__________________________________________________________________"<<endl;
-  cout<<"| Rank  |  RollNo.  |  Sem1  |  Sem2  |  Sem3  |  CGPA  |  Grade |"<<endl;
-  cout<<"------------------------------------------------------------------"<<endl;
-  int rank = 1;
-  for (const auto &student : students) {
-    cout <<"|   "<< rank << "   |   " << student.rollno << "   |   "<<student.sem1<<"    |    "<<student.sem2<<"  |   "<<student.sem3<<"    |    " << student.CGPA <<   "    |    "<< grade(student.CGPA)<<"   |"<<endl; 
-    rank++;
-  }
-cout<<"___________________________________________________________________"<<endl;
-int checkRoll;
-cout<<"Do you want get grade for specific student?\nEnter rollno. : ";
-cin>>checkRoll;
-rank=1;
-for (const auto &student : students) {
-    if(student.rollno==checkRoll){
-        cout <<"\nStudent Rollno: "<< student.rollno << "\nSemester 1    : "<<student.sem1<<"\nSemester 2    : "<<student.sem2<<"\nSemester 3    : "<<student.sem3<<"\nCGPA          : " << student.CGPA <<"\nGrade         : "<< grade(student.CGPA)<<"\nRank          : "<<rank<<endl;
+  int sel,rank = 1;
+  char loopbreak='y';
+  while(loopbreak=='y'){
+    cout<<"Enter from list below\n1.Rank list\n2.Marklist for specific student"<<endl;cin>>sel;
+  switch(sel){
+      case 1:  cout<<"__________________________________________________________________"<<endl;
+            cout<<"| Rank  |  RollNo.  |  Sem1  |  Sem2  |  Sem3  |  CGPA  |  Grade |"<<endl;
+             cout<<"------------------------------------------------------------------"<<endl;
+  
+            for (const auto &student : students) {
+             cout <<"|   "<< rank << "   |   " << student.rollno << "   |   "<<student.sem1<<"    |    "<<student.sem2<<"  |   "<<student.sem3<<"    |    " << student.CGPA <<   "    |    "<< grade(student.CGPA)<<"   |"<<endl; 
+                 rank++;
+             }
+            cout<<"___________________________________________________________________"<<endl;break;
+    case 2: int checkRoll;
+            cout<<"Enter rollno. : ";
+            cin>>checkRoll;
+            rank=1;
+            for (const auto &student : students) {
+             if(student.rollno==checkRoll){
+             cout <<"\nStudent Rollno: "<< student.rollno <<"\nRank          : "<<rank<<"\nSemester 1    : "<<student.sem1<<"\nSemester 2    : "<<student.sem2<<"\nSemester 3    : "<<student.sem3<<"\nCGPA          : " << student.CGPA <<"\nGrade         : "<< grade(student.CGPA)<<endl;
         
+            }
+            else if(student.rollno!=checkRoll){
+            rank++;
+            continue;
+            }
+            else{
+            cout<<"student rollno incorrect"<<endl;
+            }
+            }break;
+  default:cout<<"number is not in the menu";break;
     }
-    else if(student.rollno!=checkRoll){
-      rank++;
-        continue;
-    }
-    else{
-        cout<<"student rollno incorrect"<<endl;
-    }
-}
+
+    cout<<"enter 'y' to continue"<<endl;
+    cin>>loopbreak;
+    clrscr();
+  }
   return 0;
 }
